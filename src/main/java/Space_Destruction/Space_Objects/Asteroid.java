38fe2735bd9da5ea487dbc12d6eef9ae0e81728b
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2026-02-26 14:49:38 -0800

    space object update

--- src/main/java/Space_Destruction/Space_Objects/Asteroid.java
+++ src/main/java/Space_Destruction/Space_Objects/Asteroid.java
@@ -1,4 +1,29 @@
 package Space_Destruction.Space_Objects;
+import java.awt.*;
+public abstract class SpaceObjects {
+    private int x;
+    private int y;
+    private int radius;
+    private Color color;
+    private int voidSize;
 
-public class Asteroid {
-}
+
+    public Color getColor(){
+        return color;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getR(){
+        return radius;
+    }
+    public int getDistanceTo(int x2, int y2){
+        return (int) Math.sqrt((x-x2)*(x-x2)+(y-y2)*(y-y2));
+    }
+    public int getVoidSize(){
+        return voidSize;
+    }
+
