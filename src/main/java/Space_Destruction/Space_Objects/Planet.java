22565960897b67fb7a8aad559b681e0660866ff3
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2026-02-26 16:10:33 -0800

    Planet updated

--- src/main/java/Space_Destruction/Space_Objects/Planet.java
+++ src/main/java/Space_Destruction/Space_Objects/Planet.java
@@ -4,6 +4,6 @@ public class Planet extends Planetoid {
 
     public Planet(int x, int y, int radius, int voidSize, double distanceFromStar) {
         super(x, y, radius, voidSize, distanceFromStar);
-    }
+
     }
 }
