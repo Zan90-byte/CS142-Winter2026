c5d4e66e470edbe41b32bb69207eb4c052177e2f
Nrich <Nrichmond2004@gmail.com>
2026-02-26 16:12:14 -0800

    initial update of DestructionMap

--- src/main/java/Space_Destruction/DestructionMap.java
+++ src/main/java/Space_Destruction/DestructionMap.java
@@ -1,19 +1,45 @@
 package Space_Destruction;
-
-import Space_Destruction.Space_Objects.SpaceObjects;
+import Space_Destruction.Space_Objects.*;
+//import Space_Destruction.Space_Objects.SpaceObjects;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DestructionMap {
     private int scale;
     private List<SpaceObjects> objects;
     private List<SpaceVoid> voids;
 
-    public DestructionMap(int scale){
+    public DestructionMap(int scale, int numPlanets, int numAsteroid){
         this.scale = scale;
         objects = new ArrayList<SpaceObjects>();
         voids = new ArrayList<SpaceVoid>();
+        fill(numPlanets, numAsteroid);
+    }
+
+    private void fill(int numPlanets, int numAsteroids){
+        int p = 0;
+        int a = 0;
+        objects.add(new Star());
+        while(a < numAsteroids || p < numPlanets){
+            Planetoid temp;
+            if (a >= numAsteroids){
+//                temp = new Planet();
+                p ++;
+            } else if (p >= numPlanets){
+//                temp = new Asteroid();
+                a ++;
+            } else {
+                if (new Random().nextBoolean()){
+//                    temp = new Planet();
+                    p++;
+                } else {
+                    //add asteroids
+                }
+            }
+//            objects.add(temp);
+        }
     }
 
     public void update(){
