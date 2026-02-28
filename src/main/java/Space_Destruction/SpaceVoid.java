b4eb2a62521bb4332709051dd3f159bb2ca974c2
Sahana Prabhu <sahana.prabhu.r@gmail.com>
2026-02-28 11:56:08 -0800

    implemented  Asteroid and SpaceVoid classes

--- src/main/java/Space_Destruction/SpaceVoid.java
+++ src/main/java/Space_Destruction/SpaceVoid.java
@@ -1,4 +1,29 @@
 package Space_Destruction;
 
 public class SpaceVoid {
+    private int x;
+    private int y;
+    private int r;
+    private int maxRad;
+
+
+    public SpaceVoid(int x, int y, int maxRad){
+        this.x = x;
+        this.y = y;
+        this.r = 0;
+        this.maxRad = maxRad;
+    }
+
+    public void update(){
+        if (r < maxRad){
+            r++;
+        }
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getR() { return r; }
+    public int getMax() { return maxRad; }
+
 }
+
--- src/main/java/Space_Destruction/Space_Objects/Asteroid.java
+++ src/main/java/Space_Destruction/Space_Objects/Asteroid.java
@@ -1,4 +1,29 @@
 package Space_Destruction.Space_Objects;
 
-public class Asteroid {
+import java.awt.Color;
+import java.util.Random;
+
+public class Asteroid extends Planetoid{
+
+    private static final Random rand = new Random();
+
+    //constructor: x, y = actual map position (center + offset from star), orbitRadius = distance from star
+    //orbitRadius = 5 - 10 and voidSize = 25 - 50
+    public Asteroid(int x, int y, int orbitRadius){
+        super(x, y, 5 + rand.nextInt(6), 25 + rand.nextInt(26), orbitRadius);
+        this.color = randomColor();
+    }
+
+    // returns random color - grey/brown/dark grey
+    private Color randomColor(){
+        int choice = rand.nextInt(3);
+        if(choice == 0){ return Color.GRAY;}
+        if(choice == 1){ return new Color(101, 67, 37);}
+        return Color.DARK_GRAY;
+    }
+
+    @Override
+    public String toString(){
+        return "A";
+    }
 }
