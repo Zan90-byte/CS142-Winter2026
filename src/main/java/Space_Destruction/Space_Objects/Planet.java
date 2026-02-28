cc454359f803e38cce1522d1b8b9362345bf763e
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2026-02-28 14:19:41 -0800

    Planet updated

--- src/main/java/Space_Destruction/Space_Objects/Planet.java
+++ src/main/java/Space_Destruction/Space_Objects/Planet.java
@@ -2,13 +2,12 @@ package Space_Destruction.Space_Objects;
 
 import java.util.Random;
 
-public abstract class Planet extends Planetoid {
+public class Planet extends Planetoid {
 
     private static Random rand = new Random();
 
     public Planet(int x, int y, int radius, int voidSize, double distanceFromStar) {
         super(x, y, radius, voidSize, distanceFromStar);
-
 
 
     }
