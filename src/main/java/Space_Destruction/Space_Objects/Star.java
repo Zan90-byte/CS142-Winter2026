c1e64cc75de970bbec969d54c890113dc18fc38f
Nrich <Nrichmond2004@gmail.com>
2026-03-06 15:33:04 -0800

    made changes to color of central Star to be more colorful

--- src/main/java/Space_Destruction/Space_Objects/Star.java
+++ src/main/java/Space_Destruction/Space_Objects/Star.java
@@ -2,6 +2,7 @@
 
 package Space_Destruction.Space_Objects;
 import java.awt.*; // Color class to make star yellow
+import java.util.Random;
 
 public class Star extends SpaceObjects { // Inherits all fields from SpaceObject
     // Inherited fields: x, y, radius, color, voidSize
@@ -12,11 +13,18 @@ public class Star extends SpaceObjects { // Inherits all fields from SpaceObject
         x = 0; // Places Star at center of map (all orbits relative to this (0,0))
         y = 0;
         radius = 30; // Set radius to 30 pixels
-        color = new Color(255, 255, 175); // Sets color to yellow
+        color = randomColor(); // Sets color to yellow
         voidSize = 100; // Destruction wave set
     }
     public String toString(){
         return "*";
     } // Returns textual * representation (debugging)
 
+    private Color randomColor(){
+        int choice = new Random().nextInt(4);
+        if(choice == 0){ return Color.YELLOW;}
+        if(choice == 1){ return new Color(255, 255, 175);}
+        if(choice == 2){ return Color.WHITE;}
+        return new Color(175,0, 0);
+    }
 }
